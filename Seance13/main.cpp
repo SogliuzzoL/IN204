3ad42259bc6c2a2b83385f3d911efb89ba671ea4
Loris SOGLIUZZO <loris@sogliuzzo.com>
2025-12-08 11:24:19 +0100

    Add math module with factorial function and main program to demonstrate usage

---
+++ Seance13/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+import math;
+
+int main() {
+  long result = fact(20);
+  std::cout << "Factorial of 20 is " << result << std::endl;
+  return 0;
+}
---
+++ Seance13/math.ixx
@@ -0,0 +1,3 @@
+export module math;
+
+export double fact(int n) { return n == 0 ? 1 : n * fact(n - 1); }
